0bffaf82733103585de3399e5a114ec272d8691e
Lucas Casco <cascolucasisaias@gmail.com>
2026-08-31 01:53:50 -0300

    update home.nix

--- home.nix
+++ home.nix
@@ -12,6 +12,7 @@ in
     # cli i use constantly
     ripgrep   # fast search
     fd        # fast find
+    eza       # better ls
     fzf       # fuzzy finder
     jq        # json on the command line
     lazygit
@@ -22,7 +23,37 @@ in
 
   fonts.fontconfig.enable = true;
   home.sessionVariables.EDITOR = "nvim";
-  
-  home.file.".config/wezterm".source =
-    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/wezterm";
+
+  programs.fnm.enable = true;
+  programs.zoxide.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    profileExtra = ''
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    '';
+    autosuggestion.enable = true;      # ghost text from history
+    syntaxHighlighting.enable = true;  # commands turn green when valid
+    initContent = ''
+      bindkey '^f' autosuggest-accept
+    '';
+    shellAliases = {
+      ".." = "cd ..";
+      add = "git add .";
+      push = "git push";
+      pull = "git pull";
+      main = "git switch main";
+      develop = "git switch develop";
+      cc = "claude --dangerously-skip-permissions";
+      ls = "eza";
+      ll = "eza -la";
+      la = "eza -a";
+      l = "eza -l";
+      lt = "eza -TL 2";
+      cd = "z";
+    };
+   };
+
+   home.file.".config/wezterm".source =
+     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/wezterm";
 }
